a89443fe59277775dab6ad8a81e322b9844de7e0
Tony Tsui <tsui.tony@gmail.com>
2016-05-13 21:31:02 +0100

    Wrap test in sinon.test() to remove need for manual clean up.

--- test/dashboard-test.js
+++ test/dashboard-test.js
@@ -4,13 +4,11 @@ import * as thermometer from "../src/thermometer";
 import * as dashboard from "../src/dashboard";
 
 describe("Dashboard", function() {
-  it("format temperature with Celsius scale", function() {
-    sinon.stub(thermometer, "getTemperature");
+  // Wrap the test in sinon.test() so that any methods stubbed are restored after test run.
+  it("format temperature with Celsius scale", sinon.test(function() {
+    this.stub(thermometer, "getTemperature");
     thermometer.getTemperature.returns(21);
 
     expect(dashboard.formatTemperature()).to.eql("21° C");
-
-    // IMPORTANT: Undo stubbing or elses getTemperature() will be stubbed in other tests.
-    thermometer.getTemperature.restore();
-  });
+  }));
 });
